17f563db06e2f4dc8279cdf408d3bd139b6da50a
AlexandreFel1pe <alexandre.borges900@gmail.com>
2025-11-25 13:51:43 -0300

    first rosary screen

--- lib/views/rosary/rosary_screen.dart
+++ lib/views/rosary/rosary_screen.dart
@@ -9,10 +9,52 @@ class RosaryScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Rosary'),
       ),
-      body: Column(
-        children: [
-          Text('Body'),
-        ],
+      body: Center(
+        child: SingleChildScrollView(
+          padding: EdgeInsets.symmetric(
+            vertical: 10,
+          ),
+          child: Container(
+            margin: EdgeInsets.only(
+              right: 15,
+            ),
+            child: Column(
+              spacing: 5,
+              children: [
+                Text('Pelo Sinal da Santa Cruz...'),
+                Text('Oferecimento'),
+                Text('Credo'),
+                Text('Pai Nosso'),
+                Text('Três Ave Marias'),
+                Text('Gloria'),
+                Text('Mistério'),
+                Text('Pai Nosso'),
+                Text('10 Ave Marias'),
+                Text('Gloria'),
+                Text('Mistério'),
+                Text('Pai Nosso'),
+                Text('10 Ave Marias'),
+                Text('Gloria'),
+                Text('Mistério'),
+                Text('Pai Nosso'),
+                Text('10 Ave Marias'),
+                Text('Gloria'),
+                Text('Mistério'),
+                Text('Pai Nosso'),
+                Text('10 Ave Marias'),
+                Text('Gloria'),
+                Text('Mistério'),
+                Text('Pai Nosso'),
+                Text('10 Ave Marias'),
+                Text('Gloria'),
+                Text('Agradecimento'),
+                Text('Salve Rainha'),
+                Text('Ladainha'),
+                Text('Estivemos e Estaremos...'),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
